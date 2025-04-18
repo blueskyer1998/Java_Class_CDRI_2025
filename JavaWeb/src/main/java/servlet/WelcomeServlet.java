d06c70f3865186ef16cd38a4d66d220aa288017c
blueskyer1998 <litpm1998@gmail.com>
2025-04-18 11:22:09 +0800

    servlet setting

--- JavaWeb/src/main/java/servlet/WelcomeServlet.java
+++ JavaWeb/src/main/java/servlet/WelcomeServlet.java
@@ -1,5 +1,23 @@
 package servlet;
 
-public class WelcomeServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 設定編碼
+		resp.setCharacterEncoding("UTF-8"); // 給servlet看
+		resp.setContentType("text/html;charset=UTF-8"); // 給瀏覽器看
+		
+		resp.getWriter().print("歡迎光臨");
+	}
+	
 }
